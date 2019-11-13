d4c789ab48bb6eecadd1bd2743287083ab0311ce
David <Jorge.David.Mares@ibm.com>
2019-11-13 12:42:44 -0600

    Routes and response

--- API/app.js
+++ API/app.js
@@ -5,17 +5,36 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+//
+//Load routes
+//
 
-//Cargar routes
 
+//
 //middleware
-
+//
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+//
 //cors
+//
 
+
+//
 //routes
+//
+app.get('/', function(req, res){
+  res.status(200).send({
+      message:"Hello World"
+  });
+});
+
+app.get('/pruebas', function(req, res){
+  res.status(200).send({
+      message:"Test response server route NodeJS"
+  });
+});
 
 
 //export
