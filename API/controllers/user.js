f56990917dc1fac3044b07b6e7c3ce39574ec537
David <Jorge.David.Mares@ibm.com>
2019-12-17 13:14:31 -0600

    Code refactor for user controller on saveUser function

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -2,6 +2,7 @@
 
 var User = require('../models/user');
 var bcrypt = require('bcrypt-nodejs');
+var userRes = require('../response/response.js');
 
 
 function home (req, res){
@@ -20,37 +21,31 @@ function saveUser(req, res){
   var params = req.body;
   var user = new User();
 
-  if(params.name && params.surname && params.password){
+  if(!params.name || !params.email || !params.surname || !params.password || !params.nick){
+      return res.status(200).send({
+                message: userRes.userResponse(400)
+              });
+    }
 
-    user.name = params.name;
-    user.surname = params.surname;
-    user.nick = params.nick;
-    user.email = params.email;
+  user.name = params.name;
+  user.surname = params.surname;
+  user.nick = params.nick;
+  user.email = params.email;
+  user.role='ROLE_USER';
+  user.image = null;
 
-    user.role='ROLE_USER';
-    user.image = null;
+  bcrypt.hash(params.password, null, null, (err, hash) => {
+    user.password = hash;
 
-    bcrypt.hash(params.password, null, null, (err, hash) => {
-      user.password = hash;
+    user.save((err, userStored) => {
+      if(err) return res.status(500).send({message: userRes.userResponse(500)});
+      if(!userStored){
+        res.status(404).send({message: userRes.userResponse(404)})
+      }
 
-      user.save((err, userStored) => {
-        if(err) return res.status(500).send({message: 'Error trying to save user'});
-
-        if(userStored){
-          res.status(200).send({user:userStored});
-        }
-        else{
-          res.status(404).send({message: 'User have not been register'})
-        }
-      });
-
+      res.status(200).send({user:userStored});
     });
-  }
-  else{
-    res.status(200).send({
-      message: 'please, fill all the label'
-    });
-  }
+  });
 
 }
 
